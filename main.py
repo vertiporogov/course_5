c7062f6a2e455f2cd61e0c7fa7db037f9af2e149
alexander <vertiporogov.alexandr@mail.ru>
2023-12-08 22:38:42 +0300

    created get_info_company() and get_info_vacancy()

--- main.py
+++ main.py
@@ -2,22 +2,50 @@ from requests import get
 import json
 
 
-def get_info():
-    word = 'python'
-    hh_api = "https://api.hh.ru/employers/15478"
-    # params = {
-    #     'text': f'NAME:{word}',
-    #     'area': 2,  # Поиск в зоне
-    #     'page': 0,
-    #     'per_page': 100  # Кол-во вакансий на 1 странице
-    # }
+def get_info_company(id_company) -> str:
+
+    hh_api = f"https://api.hh.ru/employers/{id_company}"
     response = get(hh_api)
-    vacancies = json.loads(response.content.decode())
+    company = json.loads(response.content.decode())
+    company_dict = {
+        'company_id': company['id'],
+        'company_name': company['name'],
+        'company_url': company['alternate_url'],
+        'vacncies_url': company['vacancies_url']
+                    }
 
-    return vacancies
+    return company_dict
 
-print(get_info())
+
+def get_info_vacancy(vacancy_url) -> str:
+
+    vacancy_list = []
+
+    hh_api = vacancy_url
+    response = get(hh_api)
+    vacancy = json.loads(response.content.decode())
+
+    for i in vacancy['items']:
+        vacancy_dict = {
+            'vacancy_id': i['id'],
+            'vacancy_name': i['name'],
+            'salary': i['salary']
+                        }
+        vacancy_list.append(vacancy_dict)
+
+    return vacancy_list
+
+# for i in get_info_vacancy('https://api.hh.ru/vacancies?employer_id=2104700')['items']:
+#     print(i)
+#     print('*' * 100)
+
+# print(get_info_vacancy('https://api.hh.ru/vacancies?employer_id=1740'))
+
+for i in get_info_vacancy('https://api.hh.ru/vacancies?employer_id=1740'):
+    print(i)
+    print('*' * 100)
 
 # for i in get_info()['items']:
 #     print(i)
 #     print('*' * 100)
+
